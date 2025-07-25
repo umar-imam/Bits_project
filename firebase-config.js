5379a6eee7bd030e26f096f431949083221b529d
umar-imam <93287818+umar-imam@users.noreply.github.com>
2025-07-25 19:08:42 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdePiBJiznyDb7IqhvGrk1XqTbVyOMsbw",
+  authDomain: "bits-login.firebaseapp.com",
+  projectId: "bits-login",
+  storageBucket: "bits-login.firebasestorage.app",
+  messagingSenderId: "808799174304",
+  appId: "1:808799174304:web:8006ad7b8886420a4d93c5",
+  measurementId: "G-VHJB4BQPG4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
